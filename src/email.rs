f6ab027939d00723f3363850531332659eb36f35
Ticlext-Altihaf <yusufsekhanalthaf@gmail.com>
2024-06-07 16:53:34 +0200

    chore: Update .env.example and Cargo.toml
    - Add missing SMTP configuration options to .env.example
    - Include lettre and futures dependencies in Cargo.toml

---
+++ src/email.rs
@@ -0,0 +1,55 @@
+use std::env;
+
+use lettre::message::header::ContentType;
+use lettre::transport::smtp::authentication::Credentials;
+use lettre::{Message, SmtpTransport, Transport};
+
+pub async fn send_mail(subject: &str, body: &str) -> Result<(), String> {
+    let smtp_username_res = env::var("SMTP_USERNAME");
+    let smtp_username;
+    if smtp_username_res.is_ok() {
+        smtp_username = smtp_username_res.unwrap();
+    }else{
+        smtp_username = String::from("");
+    }
+    let smtp_password_res = env::var("SMTP_PASSWORD");
+    let smtp_password;
+    if smtp_password_res.is_ok() {
+        smtp_password = smtp_password_res.unwrap();
+    }else{
+        smtp_password = String::from("");
+    }
+    let smtp_server = env::var("SMTP_SERVER").expect("SMTP_SERVER is not set");
+    let smtp_port = env::var("SMTP_PORT").expect("SMTP_PORT is not set");
+    let smtp_from = env::var("SMTP_FROM").expect("SMTP_FROM is not set");
+    let emails = env::var("EMAILS").expect("EMAILS is not set");
+    let emails_vec: Vec<&str> = emails.split(',').collect();
+
+    let mut email_builder = Message::builder()
+        .from(smtp_from.parse().unwrap())
+        .subject(subject)
+        .header(ContentType::TEXT_PLAIN);
+    for email in emails_vec {
+        email_builder = email_builder.cc(email.parse().unwrap());
+    }
+    let email = email_builder.body(String::from(body)).unwrap();
+
+
+    let creds = Credentials::new(smtp_username.to_string(), smtp_password.to_string());
+    let transport_builder;
+    if smtp_username.is_empty() && smtp_password.is_empty() {
+        println!("No SMTP credentials provided, using dangerous builder");
+        transport_builder = SmtpTransport::builder_dangerous(smtp_server.as_str());
+    }else{
+        transport_builder = SmtpTransport::relay(smtp_server.as_str()).unwrap();
+    }
+    let mailer = transport_builder
+        .credentials(creds)
+        .port(smtp_port.parse().unwrap())
+        .build();
+
+    match mailer.send(&email) {
+        Ok(_) => Ok(()),
+        Err(e) => Err(e.to_string()),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use chrono::{DateTime, Utc};
 use dotenv::dotenv;
 use std::env;
 mod sentry_definition;
+mod email;
 /**
  * {"id": "5461763487", "project": "berani-learning-web", "project_name": "berani-learning-web", "project_slug": "berani-learning-web", "logger": null, "level": "error", "culprit": "raven.scripts.runner in main", "message": "This is an example Python exception", "url": "https://berani.sentry.io/issues/5461763487/?referrer=webhooks_plugin", "triggering_rules": [], "event": {"event_id": "a1fdd396763241eda55f7e42178c4ed4", "level": "error", "version": "5", "type": "default", "logentry": {"formatted": "This is an example Python exception", "message": null, "params": null}, "logger": "", "modules": {"my.package": "1.0.0"}, "platform": "python", "timestamp": 1717749612.583, "received": 1717749672.5841, "environment": "prod", "user": {"id": "1", "email": "sentry@example.com", "ip_address": "127.0.0.1", "username": "sentry", "name": "Sentry", "geo": {"country_code": "AU", "city": "Melbourne", "region": "VIC"}, "sentry_user": "id:1"}, "request": {"url": "http://example.com/foo", "method": "GET", "data": {"hello": "world"}, "query_string": [["foo", "bar"]], "cookies": [["foo", "bar"], ["biz", "baz"]], "headers": [["Content-Type", "application/json"], ["Referer", "http://example.com"], ["User-Agent", "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.72 Safari/537.36"]], "env": {"ENV": "prod"}, "inferred_content_type": "application/json", "api_target": null, "fragment": null}, "contexts": {"browser": {"name": "Chrome", "version": "28.0.1500", "type": "browser"}, "client_os": {"name": "Windows", "version": "8", "type": "os"}}, "stacktrace": {"frames": [{"function": "build_msg", "module": "raven.base", "filename": "raven/base.py", "abs_path": "/home/ubuntu/.virtualenvs/getsentry/src/raven/raven/base.py", "lineno": 303, "pre_context": ["                frames = stack", "", "            data.update({", "                'sentry.interfaces.Stacktrace': {", "                    'frames': get_stack_info(frames,"], "context_line": "                        transformer=self.transform)", "post_context": ["                },", "            })", "", "        if 'sentry.interfaces.Stacktrace' in data:", "            if self.include_paths:"], "in_app": false, "vars": {"'culprit'": null, "'data'": {"'message'": "u'This is a test message generated using ``raven test``'", "'sentry.interfaces.Message'": {"'message'": "u'This is a test message generated using ``raven test``'", "'params'": []}}, "'date'": "datetime.datetime(2013, 8, 13, 3, 8, 24, 880386)", "'event_id'": "'54a322436e1b47b88e239b78998ae742'", "'event_type'": "'raven.events.Message'", "'extra'": {"'go_deeper'": [["{\"'bar'\":[\"'baz'\"],\"'foo'\":\"'bar'\"}"]], "'loadavg'": [0.37255859375, 0.5341796875, 0.62939453125], "'user'": "'dcramer'"}, "'frames'": "<generator object iter_stack_frames at 0x107bcc3c0>", "'handler'": "<raven.events.Message object at 0x107bd0890>", "'k'": "'sentry.interfaces.Message'", "'kwargs'": {"'level'": 20, "'message'": "'This is a test message generated using ``raven test``'"}, "'public_key'": null, "'result'": {"'message'": "u'This is a test message generated using ``raven test``'", "'sentry.interfaces.Message'": {"'message'": "u'This is a test message generated using ``raven test``'", "'params'": []}}, "'self'": "<raven.base.Client object at 0x107bb8210>", "'stack'": true, "'tags'": null, "'time_spent'": null, "'v'": {"'message'": "u'This is a test message generated using ``raven test``'", "'params'": []}}, "colno": null, "data": null, "errors": null, "raw_function": null, "image_addr": null, "instruction_addr": null, "addr_mode": null, "package": null, "platform": null, "source_link": null, "symbol": null, "symbol_addr": null, "trust": null, "snapshot": null, "lock": null}, {"function": "capture", "module": "raven.base", "filename": "raven/base.py", "abs_path": "/home/ubuntu/.virtualenvs/getsentry/src/raven/raven/base.py", "lineno": 459, "pre_context": ["        if not self.is_enabled():", "            return", "", "        data = self.build_msg(", "            event_type, data, date, time_spent, extra, stack, tags=tags,"], "context_line": "            **kwargs)", "post_context": ["", "        self.send(**data)", "", "        return (data.get('event_id'),)", ""], "in_app": false, "vars": {"'data'": null, "'date'": null, "'event_type'": "'raven.events.Message'", "'extra'": {"'go_deeper'": [["{\"'bar'\":[\"'baz'\"],\"'foo'\":\"'bar'\"}"]], "'loadavg'": [0.37255859375, 0.5341796875, 0.62939453125], "'user'": "'dcramer'"}, "'kwargs'": {"'level'": 20, "'message'": "'This is a test message generated using ``raven test``'"}, "'self'": "<raven.base.Client object at 0x107bb8210>", "'stack'": true, "'tags'": null, "'time_spent'": null}, "colno": null, "data": null, "errors": null, "raw_function": null, "image_addr": null, "instruction_addr": null, "addr_mode": null, "package": null, "platform": null, "source_link": null, "symbol": null, "symbol_addr": null, "trust": null, "snapshot": null, "lock": null}, {"function": "captureMessage", "module": "raven.base", "filename": "raven/base.py", "abs_path": "/home/ubuntu/.virtualenvs/getsentry/src/raven/raven/base.py", "lineno": 577, "pre_context": ["        \"\"\"", "        Creates an event from ``message``.", "", "        >>> client.captureMessage('My event just happened!')", "        \"\"\""], "context_line": "        return self.capture('raven.events.Message', message=message, **kwargs)", "post_context": ["", "    def captureException(self, exc_info=None, **kwargs):", "        \"\"\"", "        Creates an event from an exception.", ""], "in_app": false, "vars": {"'kwargs'": {"'data'": null, "'extra'": {"'go_deeper'": ["[{\"'bar'\":[\"'baz'\"],\"'foo'\":\"'bar'\"}]"], "'loadavg'": [0.37255859375, 0.5341796875, 0.62939453125], "'user'": "'dcramer'"}, "'level'": 20, "'stack'": true, "'tags'": null}, "'message'": "'This is a test message generated using ``raven test``'", "'self'": "<raven.base.Client object at 0x107bb8210>"}, "colno": null, "data": null, "errors": null, "raw_function": null, "image_addr": null, "instruction_addr": null, "addr_mode": null, "package": null, "platform": null, "source_link": null, "symbol": null, "symbol_addr": null, "trust": null, "snapshot": null, "lock": null}, {"function": "send_test_message", "module": "raven.scripts.runner", "filename": "raven/scripts/runner.py", "abs_path": "/home/ubuntu/.virtualenvs/getsentry/src/raven/raven/scripts/runner.py", "lineno": 77, "pre_context": ["        level=logging.INFO,", "        stack=True,", "        tags=options.get('tags', {}),", "        extra={", "            'user': get_uid(),"], "context_line": "            'loadavg': get_loadavg(),", "post_context": ["        },", "    ))", "", "    if client.state.did_fail():", "        print('error!')"], "in_app": false, "vars": {"'client'": "<raven.base.Client object at 0x107bb8210>", "'data'": null, "'k'": "'secret_key'", "'options'": {"'data'": null, "'tags'": null}}, "colno": null, "data": null, "errors": null, "raw_function": null, "image_addr": null, "instruction_addr": null, "addr_mode": null, "package": null, "platform": null, "source_link": null, "symbol": null, "symbol_addr": null, "trust": null, "snapshot": null, "lock": null}, {"function": "main", "module": "raven.scripts.runner", "filename": "raven/scripts/runner.py", "abs_path": "/home/ubuntu/.virtualenvs/getsentry/src/raven/raven/scripts/runner.py", "lineno": 112, "pre_context": ["    print(\"Using DSN configuration:\")", "    print(\" \", dsn)", "    print()", "", "    client = Client(dsn, include_paths=['raven'])"], "context_line": "    send_test_message(client, opts.__dict__)", "in_app": false, "vars": {"'args'": ["'test'", "'https://ebc35f33e151401f9deac549978bda11:f3403f81e12e4c24942d505f086b2cad@sentry.io/1'"], "'client'": "<raven.base.Client object at 0x107bb8210>", "'dsn'": "'https://ebc35f33e151401f9deac549978bda11:f3403f81e12e4c24942d505f086b2cad@sentry.io/1'", "'opts'": "<Values at 0x107ba3b00: {'data': None, 'tags': None}>", "'parser'": "<optparse.OptionParser instance at 0x107ba3368>", "'root'": "<logging.Logger object at 0x107ba5b10>"}, "colno": null, "data": null, "errors": null, "raw_function": null, "image_addr": null, "instruction_addr": null, "addr_mode": null, "package": null, "platform": null, "post_context": null, "source_link": null, "symbol": null, "symbol_addr": null, "trust": null, "snapshot": null, "lock": null}]}, "tags": [["browser", "Chrome 28.0.1500"], ["browser.name", "Chrome"], ["client_os", "Windows 8"], ["client_os.name", "Windows"], ["environment", "prod"], ["level", "error"], ["sentry:user", "id:1"], ["server_name", "web01.example.org"], ["url", "http://example.com/foo"]], "extra": {"emptyList": [], "emptyMap": {}, "length": 10837790, "results": [1, 2, 3, 4, 5], "session": {"foo": "bar"}, "unauthorized": false, "url": "http://example.org/foo/bar/"}, "metadata": {"title": "This is an example Python exception", "in_app_frame_mix": "system-only"}, "fingerprint": ["{{ default }}"], "hashes": ["3a2b45089d0211943e5a6645fb4cea3f"], "culprit": "raven.scripts.runner in main", "title": "This is an example Python exception", "location": null, "_ref": 4506964610318336, "_ref_version": 2, "_metrics": {"bytes.stored.event": 8272}, "nodestore_insert": 1717749673.44952, "id": "a1fdd396763241eda55f7e42178c4ed4"}}
  */
@@ -110,10 +111,21 @@ async fn send_to_discord(event: sentry_definition::SentryEvent) {
     }
 }
 
+async fn send_to_email(event: sentry_definition::SentryEvent) -> Result<(), String> {
+  
+    let subject = format!("{} - {}",  event.project_name, event.message);
+    let body = format!("Culprit: {}\nURL: {}", event.culprit, event.url);
+    email::send_mail(&subject, &body).await
+}
+
 async fn handle_sentry(item: web::Json<sentry_definition::SentryEvent>) -> impl Responder {
     println!("Received Sentry event: {:?}", item.event.title);
     send_to_discord(item.clone()).await;
-
+    let res = send_to_email(item.clone()).await;
+    match res {
+        Ok(_) => println!("Email sent"),
+        Err(e) => println!("Error sending email: {}", e),
+    }
     "OK"
 }
 
@@ -154,6 +166,8 @@ mod tests {
     use std::fs::File;
     use std::io::{self, BufRead};
     use std::path::Path;
+    use futures::executor;
+    use dotenv::dotenv;
 
     use crate::sentry_definition::SentryEvent;
     #[test]
@@ -167,6 +181,30 @@ mod tests {
             println!("{:?}", event_res);
         }
     }
+
+    fn get_sample_event() -> Result<SentryEvent, &'static str> {
+        let path = Path::new("examples.jsonl");
+        
+        let lines_res = read_lines(path).unwrap();
+        let sample: SentryEvent;
+        for line in lines_res {
+            let event_res: SentryEvent = serde_json::from_str(&line.unwrap()).unwrap();
+            sample = event_res;
+            return Ok(sample);
+        }
+        Err("No sample event found")
+    }
+
+    #[test]
+    fn send_email() {
+        let sample = get_sample_event().unwrap();
+        // make sure env set
+        dotenv().ok();
+        let res = executor::block_on(super::send_to_email(sample));
+        assert_eq!(res, Ok(()));
+    }
+
+
 
     // The output is wrapped in a Result to allow matching on errors.
     // Returns an Iterator to the Reader of the lines of the file.
